4212089551b4e2fe7c5f4b763c57cd86cfbb1ba1
vroyt <vroyt@DESKTOP-RDTBQS9>
2017-05-13 12:13:46 -0500

    ParseContent: added some functionality and imports that may need later

--- ViakViak Sql/dbo/Clr/ParseContent.cs
+++ ViakViak Sql/dbo/Clr/ParseContent.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using Microsoft.SqlServer.Server;
 using System.Diagnostics;
+using System.Xml;
 
 public partial class StoredProcedures
 {
@@ -13,12 +14,31 @@ public partial class StoredProcedures
     /// <param name="content"></param>
     /// <remarks>See <see cref="https://www.codeproject.com/Tips/841439/Create-Run-Debug-and-Deploy-SQL-CLR-Function-with"/>Article "Create, Run, Debug and Deploy SQL CLR Function with Visual Studio 2013 Database Project"</remarks>
     [SqlProcedure]
-    public static void ParseContent(SqlString content)
+    public static void ParseContent(int articleID, SqlString content)
     {
         string contentText = content.Value;
         if (String.IsNullOrEmpty(contentText)) return;
 
         contentText = "<content>" + contentText + "</content>";
         Debug.WriteLine(contentText);
+
+        XmlDocument xmlDoc = new XmlDocument();
+        try
+        {
+            xmlDoc.LoadXml(contentText);
+
+            using (var conn = new SqlConnection("context connection=true"))
+            {
+                conn.Open();
+                
+                  
+            }
+        }
+        catch(Exception ex)
+        {
+            Debug.Write("Error: " + ex.Message + "\n" + ex.StackTrace);
+        }
     }
+
+    
 }
