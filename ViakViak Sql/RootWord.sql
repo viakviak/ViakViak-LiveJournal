ee3f15e2815de94c224bd2e52b0ca041e29ffa64
vroyt <vroyt@DESKTOP-RDTBQS9>
2017-05-13 21:53:33 -0500

    Added RootWord table

---
+++ ViakViak Sql/RootWord.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[RootWord]
+(
+	[RootWordID] INT NOT NULL PRIMARY KEY, 
+    [RootID] INT NOT NULL, 
+    [WordID] INT NOT NULL, 
+    [CreateOn] DATETIME NOT NULL DEFAULT getdate(), 
+    CONSTRAINT [FK_RootWord_ToRoot] FOREIGN KEY (RootID) REFERENCES [Root](RootID), 
+    CONSTRAINT [FK_RootWord_ToWord] FOREIGN KEY (WordID) REFERENCES Word(WordID)
+)
