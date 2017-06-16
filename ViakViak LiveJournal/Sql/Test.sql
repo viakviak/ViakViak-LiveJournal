bf2f72537391773d249bcefa6856729898f2f699
viakviak <viakviak@outlook.com>
2017-06-16 16:39:20 -0500

    Update Test.sql
    Added IsTypeOf, uncommented inserts and translations, corrected spAddTranslation

--- ViakViak LiveJournal/Sql/Test.sql
+++ ViakViak LiveJournal/Sql/Test.sql
@@ -1,4 +1,4 @@
-﻿-- Test
+-- Test
 
 IF Object_ID(N'dbo.Entity') IS NOT NULL
 	DROP TABLE dbo.Entity
@@ -17,7 +17,9 @@ GO
 IF OBJECT_ID('spAddTranslation') IS NOT NULL
 	DROP PROCEDURE dbo.spAddTranslation
 GO
-
+IF OBJECT_ID('IsTypeOf') IS NOT NULL
+	DROP FUNCTION dbo.IsTypeOf
+GO
 CREATE TABLE dbo.Entity (
 	EntityID int IDENTITY(1,1) NOT NULL,
 	TypeID int NOT NULL, -- EntityID of type
@@ -99,6 +101,7 @@ GO
 
 /*
 	Adds translation of the same type as the specified existing item
+	Returns Collection ID or negative incorrect argument number
 */
 CREATE PROCEDURE spAddTranslation
 	@translationOfID int,
@@ -114,11 +117,11 @@ AS
 	SET NOCOUNT ON;
 
 	if @translationOfID IS NULL OR @translationOfID <= 0
-		return 1;
+		return -1;
 	if @translationLanguageID IS NULL OR @translationLanguageID <= 0
-		return 2;
+		return -2;
 	if @translation IS NULL OR LEN(@translation) <= 0
-		return 3;
+		return -3;
 
 	SELECT	@collectionID = CollectionID, @itemTypeID = ItemTypeID
 	FROM	dbo.CollectionItem
@@ -144,14 +147,45 @@ AS
 	end
 GO
 
+/*	-- Tests:
+	SELECT 5, 3, dbo.IsTypeOf(5, 3);
+	SELECT 5, 1, dbo.IsTypeOf(5, 1);
+	SELECT NULL, 1, dbo.IsTypeOf(NULL, 1);
+	SELECT 1001, 2, dbo.IsTypeOf(1001, 2);
+*/
+CREATE FUNCTION IsTypeOf(
+	@entityID as int,
+	@typeID as int
+) returns bit as
+	begin
+	if @entityID IS NULL OR @typeID IS NULL
+		return 0;
+	if @typeID = 1
+		return 1;
+
+	DECLARE @entityTypeID as int = NULL;
+
+	while 1=1
+		begin
+		SELECT @entityTypeID = TypeID FROM dbo.Entity WHERE EntityID = @entityID;
+		if @entityTypeID IS NULL OR @entityTypeID < @typeID
+			return 0;
+
+		if @entityTypeID = @typeID
+			return 1;
+		SET @entityID = @entityTypeID;
+		end
+	
+	return 0;
+	end
+GO
+
 SET IDENTITY_INSERT dbo.Entity ON
 INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (1, 1, N'Type');
 INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (2, 1, N'Language');
 INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (3, 1, N'Collection'); -- collection of items
 INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (4, 3, N'Translation');-- collection of translations
 INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (5, 3, N'List');-- ordered collection
-INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (6, 5, N'Inheritance');-- list of inherited types, excluding generic "Type" with ID=1
-
 
 INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (101, 1, N'Article');
 INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (102, 1, N'Label');
@@ -163,11 +197,11 @@ INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (106, 104, N'Prefi
 INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (1001, 2, N'English');
 INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (1002, 2, N'Russian');
 ---- Bundle types
---INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (10001, 3, N'ArticleLabel');
---INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (10002, 3, N'ArticleWord');
---INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (10003, 3, N'ComponentWord');
---INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (10004, 3, N'RootWord');
---INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (10005, 3, N'PrefixWord');
+INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (10001, 3, N'ArticleLabel');
+INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (10002, 3, N'ArticleWord');
+INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (10003, 3, N'ComponentWord');
+INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (10004, 3, N'RootWord');
+INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (10005, 3, N'PrefixWord');
 
 INSERT INTO dbo.Entity (EntityID, TypeID, Translation) VALUES (10000, 2, N'System');
 
@@ -179,15 +213,14 @@ exec dbo.spAddTranslation 1, 1002, N'Тип'; -- Type in Russian
 exec dbo.spAddTranslation 2, 1002, N'Язык'; -- Language in Russian
 exec dbo.spAddTranslation 3, 1002, N'Коллекция'; -- Collection in Russian
 exec dbo.spAddTranslation 4, 1002, N'Перевод'; -- Translation in Russian
---exec dbo.spAddTranslation 101, 1002, N'Статья'; -- Article in Russian
---exec dbo.spAddTranslation 102, 1002, N'Метка'; -- Label in Russian
---exec dbo.spAddTranslation 102, 1002, N'Ярлык'; -- Label in Russian
---exec dbo.spAddTranslation 103, 1002, N'Слово'; -- Word in Russian
---exec dbo.spAddTranslation 104, 1002, N'Компонента'; -- Component in Russian
---exec dbo.spAddTranslation 105, 1002, N'Корень'; -- Root in Russian
---exec dbo.spAddTranslation 106, 1002, N'Приставка'; -- Prefix in Russian
---exec dbo.spAddTranslation 106, 1002, N'Префих'; -- Prefix in Russian
---exec dbo.spAddTranslation 1001, 1002, N'Английский'; -- English in Russian
---exec dbo.spAddTranslation 1002, 1002, N'Русский'; -- Russian in Russian
+exec dbo.spAddTranslation 101, 1002, N'Статья'; -- Article in Russian
+exec dbo.spAddTranslation 102, 1002, N'Метка'; -- Label in Russian
+exec dbo.spAddTranslation 102, 1002, N'Ярлык'; -- Label in Russian
+exec dbo.spAddTranslation 103, 1002, N'Слово'; -- Word in Russian
+exec dbo.spAddTranslation 104, 1002, N'Компонента'; -- Component in Russian
+exec dbo.spAddTranslation 105, 1002, N'Корень'; -- Root in Russian
+exec dbo.spAddTranslation 106, 1002, N'Приставка'; -- Prefix in Russian
+exec dbo.spAddTranslation 106, 1002, N'Префих'; -- Prefix in Russian
+exec dbo.spAddTranslation 1001, 1002, N'Английский'; -- English in Russian
+exec dbo.spAddTranslation 1002, 1002, N'Русский'; -- Russian in Russian
 GO
-
